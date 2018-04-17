d56d8d7090851171bc02cffabab5c6f0a83b4f37
Emma Paris <eparis@atlassian.com>
2018-04-17 17:38:19 +0530

    Initialization complete

---
+++ Sample.java
@@ -0,0 +1,11 @@
+package collection;
+
+import java.util.ArrayList;
+
+public class Sample {
+	public static void main(String[] args) {
+		Sample s;
+		ArrayList<String> list = new ArrayList<String>();
+		
+	}
+}
