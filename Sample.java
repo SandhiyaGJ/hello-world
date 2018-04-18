b9794352bf246a1aade90f93379d30882421bb6b
Kalaiarasan Sekar -X (kalsekar - WIPRO LIMITED at Cisco) <kalsekar@partnet.cisco.com>
2018-04-18 10:52:08 +0530

    Initialize collections

--- Sample.java
+++ Sample.java
@@ -4,8 +4,11 @@ import java.util.ArrayList;
 
 public class Sample {
 	public static void main(String[] args) {
-		Sample s;
 		ArrayList<String> list = new ArrayList<String>();
+		list.add("Sandy");
+		list.add("Krish");
+		list.add("Ajith");
+		list.add("Priya");
 		
 	}
 }
