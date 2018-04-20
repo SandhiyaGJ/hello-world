032a81f00c51174db07507cb1a83de5178421d35
Kalaiarasan Sekar -X (kalsekar - WIPRO LIMITED at Cisco) <kalsekar@partnet.cisco.com>
2018-04-20 11:22:54 +0530

    Init coll

---
+++ JavaFile1.java
@@ -0,0 +1,15 @@
+package collection;
+
+import java.util.ArrayList;
+
+public class Sample {
+	public static void main(String[] args) {
+		ArrayList<String> list = new ArrayList<String>();
+		list.add("Sandy");
+		list.add("Krish");
+		list.add("Ajith");
+		list.add("Priya");
+		
+	}
+	
+}
